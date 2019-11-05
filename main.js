d1343a2cad9bda1d4285b931b0e4de2d56be0f39
hoto17296 <mail@hoto.me>
2019-11-05 22:32:56 +0900

    add overlay

--- main.js
+++ main.js
@@ -2,11 +2,16 @@ var openButton = document.getElementById('openButton');
 var result = document.getElementById('result');
 var demoModal = document.getElementById('demoModal');
 var closeButton = document.getElementById('closeButton');
+var overlay = document.getElementById('overlay');
 
 openButton.addEventListener('click', function() {
   demoModal.style.display = 'block';
 });
 
 closeButton.addEventListener('click', function() {
+  demoModal.style.display = 'none';
+});
+
+overlay.addEventListener('click', function() {
   demoModal.style.display = 'none';
 });
