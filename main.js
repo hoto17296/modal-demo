d024698646d1aff63a84318619244e5353590e26
hoto17296 <mail@hoto.me>
2019-11-05 22:54:24 +0900

    add date select form

--- main.js
+++ main.js
@@ -15,3 +15,27 @@ closeButton.addEventListener('click', function() {
 overlay.addEventListener('click', function() {
   demoModal.style.display = 'none';
 });
+
+var yearForm = demoModal.querySelector('form').year;
+for (var i=0; i < 30; i++) {
+  var option = document.createElement('option');
+  option.value = 2019 - i;
+  option.textContent = option.value;
+  yearForm.appendChild(option);
+}
+
+var monthForm = demoModal.querySelector('form').month;
+for (var i=0; i < 12; i++) {
+  var option = document.createElement('option');
+  option.value = i + 1;
+  option.textContent = option.value;
+  monthForm.appendChild(option);
+}
+
+var dayForm = demoModal.querySelector('form').day;
+for (var i=0; i < 31; i++) {
+  var option = document.createElement('option');
+  option.value = i + 1;
+  option.textContent = option.value;
+  dayForm.appendChild(option);
+}
