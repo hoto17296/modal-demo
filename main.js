9ac3d92525396595b6d50be41df300d3878e3237
hoto17296 <mail@hoto.me>
2019-11-05 23:05:26 +0900

    add submit button

--- main.js
+++ main.js
@@ -8,34 +8,35 @@ openButton.addEventListener('click', function() {
   demoModal.style.display = 'block';
 });
 
-closeButton.addEventListener('click', function() {
-  demoModal.style.display = 'none';
-});
-
 overlay.addEventListener('click', function() {
   demoModal.style.display = 'none';
 });
 
-var yearForm = demoModal.querySelector('form').year;
+var modalForm = demoModal.querySelector('form');
+
 for (var i=0; i < 30; i++) {
   var option = document.createElement('option');
   option.value = 2019 - i;
   option.textContent = option.value;
-  yearForm.appendChild(option);
+  modalForm.year.appendChild(option);
 }
 
-var monthForm = demoModal.querySelector('form').month;
 for (var i=0; i < 12; i++) {
   var option = document.createElement('option');
   option.value = i + 1;
   option.textContent = option.value;
-  monthForm.appendChild(option);
+  modalForm.month.appendChild(option);
 }
 
-var dayForm = demoModal.querySelector('form').day;
 for (var i=0; i < 31; i++) {
   var option = document.createElement('option');
   option.value = i + 1;
   option.textContent = option.value;
-  dayForm.appendChild(option);
+  modalForm.day.appendChild(option);
 }
+
+modalForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  result.textContent = modalForm.year.value + '/' + modalForm.month.value + '/' + modalForm.day.value;
+  demoModal.style.display = 'none';
+});
