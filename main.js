25a7b6124e4df3a719ab4cec598e377e1bcb8146
hoto17296 <mail@hoto.me>
2019-11-09 00:00:09 +0900

    impl Modal class

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
-var openButton = document.getElementById('openButton');
-var result = document.getElementById('result');
-var demoModal = document.getElementById('demoModal');
-var closeButton = document.getElementById('closeButton');
-var overlay = document.getElementById('overlay');
-var modalForm = demoModal.querySelector('form');
+// Modal class
+function Modal(el) {
+  this.el = el;
+  this.el.querySelector('#overlay').addEventListener('click', this.close.bind(this));
+}
 
-openButton.addEventListener('click', function() {
-  demoModal.style.display = 'block';
+Modal.prototype.open = function() {
+  this.el.style.display = 'block';
+};
+
+Modal.prototype.close = function() {
+  this.el.style.display = 'none';
+};
+
+// Use Modal
+var demoModal = new Modal(document.getElementById('demoModal'));
+
+document.getElementById('openButton').addEventListener('click', function() {
+  demoModal.open();
 });
 
-overlay.addEventListener('click', function() {
-  demoModal.style.display = 'none';
-});
-
+var modalForm = document.querySelector('#demoModal form');
 modalForm.addEventListener('submit', function(event) {
   event.preventDefault();
   result.textContent = modalForm.wants.value;
-  demoModal.style.display = 'none';
+  demoModal.close();
 });
